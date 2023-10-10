aba61c4afd524581a13391ba1fad77c4f995b433
Ernest <gbaboh.ernest@gmail.com>
2023-10-10 03:43:54 +0000

    refactor the base model to re-create an instance with proper formating of date-time

--- models/base_model.py
+++ models/base_model.py
@@ -19,16 +19,24 @@ class BaseModel:
             *args => list of args, positional args
             **kwargs => key, values pairs
         """
-        if not kwargs:
-            # if an instance is created without key/value pair args
+
+        if kwargs:
+            # we are creating an instance from a dictionary
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    # if key is not __class__ then check if it is
+                    # created_at or updated_at convert the string
+                    # rep to datetime object using datetime.strptime()
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime
+                                (value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            # we are creating a new instance from the beginning
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-        else:
-            self.id = kwargs.get('id', str(uuid.uuid4()))
-            self.created_at = kwargs.get('created_at', datetime.now())
-            self.updated_at = kwargs.get('updated_at', self.created_at)
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
@@ -48,9 +56,10 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
 
-        # convert created_at, updated_at
-        # time to ISO format strings
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        # convert created_at, updated_at to string using strftime()
+        obj_dict['created_at'] = self.created_at.strftime(
+                                    '%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = self.updated_at.strftime(
+                                    '%Y-%m-%dT%H:%M:%S.%f')
 
         return obj_dict
