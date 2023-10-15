668fcd93677134a71d13fb3c319a03e021d66684
Ernest <gbaboh.ernest@gmail.com>
2023-10-15 10:53:07 +0000

    revert changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ serialization and deserialization of future instances
 
 import uuid
 from datetime import datetime
-import models
+# import models
 from models import storage
 
 
@@ -41,8 +41,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
-            # storage.new(self)
+            # models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Print a nice string representation of the class"""
@@ -68,8 +68,8 @@ class BaseModel:
         """
 
         self.updated_at = datetime.now()
-        models.storage.save()
-        # storage.save()
+        # models.storage.save()
+        storage.save()
 
     """def to_dict(self):
         #returns a dictionary containing all keys/values -
